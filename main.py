eb11477831db308e894e90d255e70e63b49c814f
devogs <billy.bob@devogs.xyz>
2025-03-06 20:05:29 +0100

    add docstring to main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+main.py
+
+The entry point script for the chess tournament management system.
+Imports and runs the controller module.
+
+Usage:
+    Run the application:
+        $ python3 main.py
+"""
+
 from controller import Controller
 
 if __name__ == "__main__":
